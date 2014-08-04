65f76f91f5226ae72283dca711827f8c2d26c974
anderschen <chenhf2010@qq.com>
2014-08-04 22:20:35 +0800

    test

---
+++ src/main/java/com/cssweb/payment/bank/citic/Test.java
@@ -0,0 +1,12 @@
+package com.cssweb.payment.bank.citic;
+
+/**
+ * Created by chenhf on 2014/8/4.
+ */
+public class Test {
+
+    public static void main(String[] args)
+    {
+        System.out.println("你好");
+    }
+}
