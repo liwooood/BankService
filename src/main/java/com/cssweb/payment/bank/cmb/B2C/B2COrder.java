26627e415d7dfc603c2b3d904c9e1da478c92017
anderschen <chenhf2010@qq.com>
2014-08-07 17:14:26 +0800

    commit

---
+++ src/main/java/com/cssweb/payment/bank/cmb/B2C/B2COrder.java
@@ -0,0 +1,66 @@
+package com.cssweb.payment.bank.cmb.B2C;
+
+/**
+ * Created by chenhf on 2014/7/30.
+ */
+public class B2COrder {
+
+
+
+    public void B2CSign()
+    {
+        // 商户密钥
+        String key = "21asfDSDD344asdA";
+        // 订单日期
+        String date = "20140731";
+        // 开户分行号
+        String branchId = "0010";
+        // 商户号
+        String cono = "000818";
+        // 订单号
+        String billNo = "0000000003";
+        // 订单金额
+        String amount = "100.00";
+        // 商户自定义参数
+        String merchantPara = "k=v";
+        // 商户接收通知
+        String merchantUrl = "http://cashier.cecpay.com/notify";
+        // 付款方用户标识
+        String payerId = "user1";
+        // 收款方用户标识
+        String payeeId = "user2";
+        // 商户取得的客户端ip
+        String clientIp = "192.168.1.1";
+        // 商品类型
+        String goodsType = "54011600"; // 网上支付
+        // 保留
+        String reserved = "";
+
+        cmb.MerchantCode mc = new cmb.MerchantCode();
+
+        String signData = mc.genMerchantCode(key, date, branchId, cono, billNo, amount, merchantPara, merchantUrl, payerId, payeeId, clientIp, goodsType, reserved);
+        System.out.println("产生的校验码为：" + signData);
+    }
+
+    public void B2CVerify()
+    {
+        String notifyResult = "http://cashier.cecpay.com/notify?Succeed=Y&CoNo=000818&BillNo=000000&Amount=100.00&Date=20140731&MerchantPara=k=v&BillExInfo=00622609******61810000000000000000000000000000000000000000000000&Msg=00100008182014073100000000000000000000&Signature=124|74|130|221|6|217|254|246|179|6|251|21|2|55|190|60|189|222|233|97|1|12|129|144|16|127|66|238|134|225|142|235|91|213|172|166|56|7|9|60|198|174|146|234|79|207|155|202|125|62|135|145|116|171|252|212|79|10|122|108|169|252|71|182|";
+        try {
+            cmb.netpayment.Security cmb = new cmb.netpayment.Security("cmb.public.key");
+            cmb.checkInfoFromBank(notifyResult.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void B2B()
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        B2COrder service = new B2COrder();
+        service.B2CSign();
+    }
+}
---
+++ src/main/java/com/cssweb/payment/bank/cmb/BankEnterprise/HttpRequest.java
@@ -0,0 +1,217 @@
+package com.cssweb.payment.bank.cmb.BankEnterprise;
+
+import java.io.*;
+import java.net.*;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * HTTP通讯范例: 直接支付
+ *
+ * @author 徐蓓
+ */
+public class HttpRequest {
+    /**
+     * 生成请求报文
+     *
+     * @return
+     */
+
+    private String toPersonal() {
+        // 构造支付的请求报文
+        XmlPacket xmlPkt = new XmlPacket("AgentRequest", "银企直连专用集团1");
+
+        Map mpPodInfo = new Properties();
+        mpPodInfo.put("BUSCOD", "N03020");
+        mpPodInfo.put("BUSMOD", "00001");
+        //mpPodInfo.put("C_TRSTYP", "");
+        mpPodInfo.put("TRSTYP", "BYBK");
+        mpPodInfo.put("DBTACC", "591902896710201");
+        mpPodInfo.put("BBKNBR", "59");
+        mpPodInfo.put("SUM", "100.00");
+        mpPodInfo.put("TOTAL", "1");
+        mpPodInfo.put("YURREF", "0000000001"); // 流水号或订单号
+        mpPodInfo.put("MEMO", "提现");
+        xmlPkt.putProperty("SDKATSRQX", mpPodInfo);
+
+        Map mpPayInfo = new Properties();
+        mpPayInfo.put("ACCNBR", "6225885910000066");
+        mpPayInfo.put("CLTNAM", "曾亮");
+        mpPayInfo.put("TRSAMT", "100.00");
+        xmlPkt.putProperty("SDKATDRQX", mpPayInfo);
+
+        return xmlPkt.toXmlString();
+    }
+    private void toPersonalResult(String result) {
+        if (result != null && result.length() > 0) {
+            XmlPacket pktRsp = XmlPacket.valueOf(result);
+            if (pktRsp != null) {
+                String sRetCod = pktRsp.getRETCOD();
+                if (sRetCod.equals("0")) {
+                    Map propPayResult = pktRsp.getProperty("NTREQNBRY", 0);
+                    String sREQNBR = (String) propPayResult.get("REQNBR");
+
+                   /*
+                    if (sREQSTS.equals("FIN") && sRTNFLG.equals("F")) {
+                        System.out.println("支付失败："
+                                + propPayResult.get("ERRTXT"));
+                    } else {
+                        System.out.println("支付已被银行受理（支付状态：" + sREQSTS + "）");
+                    }
+                    */
+                } else if (sRetCod.equals("-9")) {
+                    System.out.println("支付未知异常，请查询支付结果确认支付状态，错误信息："
+                            + pktRsp.getERRMSG());
+                } else {
+                    System.out.println("支付失败：" + pktRsp.getERRMSG());
+                }
+            } else {
+                System.out.println("响应报文解析失败");
+            }
+        }
+    }
+
+    private String searchAccount() {
+        // 构造支付的请求报文
+        XmlPacket xmlPkt = new XmlPacket("GetAccInfo", "商户41");
+
+        Map mpPodInfo = new Properties();
+        mpPodInfo.put("BBKNBR", "59");
+        mpPodInfo.put("ACCNBR", "591902897710601");
+        xmlPkt.putProperty("SDKACINFX", mpPodInfo);
+
+
+
+        return xmlPkt.toXmlString();
+    }
+
+    /**
+     * 连接前置机，发送请求报文，获得返回报文
+     *
+     * @param data
+     * @return
+     * @throws MalformedURLException
+     */
+    private String sendRequest(String data) {
+        String result = "";
+        try {
+            URL url;
+            url = new URL("http://localhost:8080");
+
+            HttpURLConnection conn;
+            conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            OutputStream os;
+            os = conn.getOutputStream();
+            os.write(data.toString().getBytes("UTF-8"));
+            os.close();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn
+                    .getInputStream()));
+            String line;
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+
+            System.out.println(result);
+            br.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * 处理返回的结果
+     *
+     * @param result
+     */
+    private void searchAccountResult(String result) {
+        if (result != null && result.length() > 0) {
+            XmlPacket pktRsp = XmlPacket.valueOf(result);
+            if (pktRsp != null) {
+                String sRetCod = pktRsp.getRETCOD();
+                if (sRetCod.equals("0")) {
+                    Map propPayResult = pktRsp.getProperty("NTQACINFZ", 0);
+                    String sCCYNBR = (String) propPayResult.get("CCYNBR");
+                    String sACCITM = (String) propPayResult.get("ACCITM");
+                    String sBBKNBR = (String) propPayResult.get("BBKNBR");
+                    String sACCNBR = (String) propPayResult.get("ACCNBR");
+                    String sACCNAM = (String) propPayResult.get("ACCNAM");
+                    String sACCBLV = (String) propPayResult.get("ACCBLV");
+                    String sONLBLV = (String) propPayResult.get("ONLBLV");
+                    String sSTSCOD = (String) propPayResult.get("STSCOD");
+                    String sOPNDAT = (String) propPayResult.get("OPNDAT");
+                   /*
+                    if (sREQSTS.equals("FIN") && sRTNFLG.equals("F")) {
+                        System.out.println("支付失败："
+                                + propPayResult.get("ERRTXT"));
+                    } else {
+                        System.out.println("支付已被银行受理（支付状态：" + sREQSTS + "）");
+                    }
+                    */
+                } else if (sRetCod.equals("-9")) {
+                    System.out.println("支付未知异常，请查询支付结果确认支付状态，错误信息："
+                            + pktRsp.getERRMSG());
+                } else {
+                    System.out.println("支付失败：" + pktRsp.getERRMSG());
+                }
+            } else {
+                System.out.println("响应报文解析失败");
+            }
+        }
+
+        /*
+        if (result != null && result.length() > 0) {
+            XmlPacket pktRsp = XmlPacket.valueOf(result);
+            if (pktRsp != null) {
+                String sRetCod = pktRsp.getRETCOD();
+                if (sRetCod.equals("0")) {
+                    Map propPayResult = pktRsp.getProperty("NTQPAYRQZ", 0);
+                    String sREQSTS = (String) propPayResult.get("REQSTS");
+                    String sRTNFLG = (String) propPayResult.get("RTNFLG");
+                    if (sREQSTS.equals("FIN") && sRTNFLG.equals("F")) {
+                        System.out.println("支付失败："
+                                + propPayResult.get("ERRTXT"));
+                    } else {
+                        System.out.println("支付已被银行受理（支付状态：" + sREQSTS + "）");
+                    }
+                } else if (sRetCod.equals("-9")) {
+                    System.out.println("支付未知异常，请查询支付结果确认支付状态，错误信息："
+                            + pktRsp.getERRMSG());
+                } else {
+                    System.out.println("支付失败：" + pktRsp.getERRMSG());
+                }
+            } else {
+                System.out.println("响应报文解析失败");
+            }
+        }
+        */
+    }
+
+    public static void main(String[] args) {
+        try {
+            HttpRequest request = new HttpRequest();
+
+            // 生成请求报文
+            String data = request.toPersonal();
+            // 连接前置机，发送请求报文，获得返回报文
+            String result = request.sendRequest(data);
+            // 处理返回的结果
+            request.toPersonalResult(result);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
